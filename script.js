4f4fe5d998eee3157061a0c9c03309ca3847d7b6
kenzelsherbini <526kelsherbini@frhsd.com>
2024-11-06 10:42:29 -0500

    story

---
+++ script.js
@@ -0,0 +1,9 @@
+let userName = prompt('What is your name?')
+let userAge = prompt('How old are you?')
+console.log(userName)
+console.log(userAge)
+
+let myStory = `Hello ${userName}, How are you doing today? How does it feel to be ${userAge}  years old?`
+console.log(myStory)
+
+document.getElementById('story').innerHTML = myStory
